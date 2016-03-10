c5338d566ba92134ccd98edca0357f2fae0a4cc8
Pavel Rybintsev <punkihoy86@gmail.com>
2016-03-10 19:07:51 +0000

    Added continious door sencing

--- DoorSensor/control.py
+++ DoorSensor/control.py
@@ -7,14 +7,22 @@ import RPi.GPIO as GPIO
 class DoorSensor(object):
 
     def __init__(self):
-        self.input_channel = 11
+        self.input_channel = 40
         self.time_stamp = time.time()
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.input_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def listen_to_edge(self):
-        channel = GPIO.wait_for_edge(self.input_channel, GPIO.FALLING)
-        print(channel)
+        while True:
+            channel = GPIO.wait_for_edge(self.input_channel, GPIO.BOTH)
+            time_now = time.time()
+            if time_now - self.time_stamp > 1:
+                input_state = GPIO.input(self.input_channel)
+                if input_state == GPIO.LOW:
+                    print('Door closed')
+                else:
+                    print('Door opened')
+
 
 if __name__ == '__main__':
     try:
